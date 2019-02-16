91740824ec281788616658f9b0f291752ca268c7
nziokaivy <nziokaivy@gmail.com>
2019-02-17 00:38:42 +0300

    Add logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,6 +8,8 @@ from .. import db
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username = form.username.data, email = form.email.data, password = form.password.data)
@@ -21,16 +23,17 @@ def register():
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and form.password.data:
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('main.home'))
+            return redirect(next_page) if next_page else redirect(url_for('main.index'))
         else:    
             flash('Login unsuccessful.Please check email and password!', 'danger')    
-        return redirect(url_for('auth.login'))
     return render_template('auth/login.html', title='Login', form=form)
 
 @auth.route('/logout')
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from .import db
+from .import db, login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
@@ -9,7 +9,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
      __tablename__ = 'users'
      id = db.Column(db.Integer, primary_key=True)
      username = db.Column(db.String(20), unique=True, nullable=False)
