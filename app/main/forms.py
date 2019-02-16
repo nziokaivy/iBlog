571e4daa6e246fe32e45ac3f0c3e41e9d816429c
nziokaivy <nziokaivy@gmail.com>
2019-02-17 01:50:06 +0300

    add update profile template

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from ..models import User, Post
+
+
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
     submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -2,13 +2,9 @@ from flask_wtf import FlaskForm
 from flask import Flask, render_template, url_for,redirect, request, abort
 from . import main
 from ..models import User
-
-from flask import render_template,request,redirect,url_for,abort
-from . import main
-from ..import db
-from ..models import User
 from flask_login import login_user,logout_user,login_required, current_user
-from .. import db
+from .forms import UpdateProfile
+from ..import db
 
 @main.route("/")
 @main.route("/home")
@@ -31,3 +27,22 @@ def profile(username):
         abort(404)
 
     return render_template('account.html', user = user)
+
+@main.route('/user/<username>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(username):
+    user = User.query.filter_by(username = username).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        current_user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.account',username=current_user.username))
+
+    return render_template('update.html',form =form)    
